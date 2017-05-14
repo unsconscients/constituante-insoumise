cefe4e32123cc9912c66c99bebbcef090932e121
MarcusMac <marc.muller@marcosoft.fr>
2017-05-14 17:34:06 +0200

    Ajout de la page Proposition

---
+++ application/controllers/Proposer.php
@@ -0,0 +1,51 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Proposer extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see https://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+
+		$this->load->helper('url');
+
+		$this->load->view('header');
+
+		$this->load->view('proposer');
+
+			echo '<h1>Proposer</h1>';
+
+		$this->load->view('footer');
+	}
+
+
+	public function propose()
+	{
+
+		if($this->input->post(''))
+		$this->load->helper('url');
+
+		$this->load->view('header');
+
+		$this->load->view('proposer');
+
+			echo '<h1>Proposer</h1>';
+
+		$this->load->view('footer');
+	}
+
+}
---
+++ application/views/proposer.php
@@ -0,0 +1,67 @@
+<div class="container">
+
+  <div class="col-lg-8 col-lg-offset-2 col-xs-12">
+
+    <div class="jumbotron jumbotron-insoumis">
+      <h1>Faites une proposition !</h1>
+      <p>Petit text .... </p>
+    </div>
+
+    <form action="<?php echo base_url('proposer/propose')?>" method="post">
+
+      <div class="panel panel-default panel-insoumis">
+        <div class="panel-heading">
+          <h3 class = "panel-title">Coordonnés</h3>
+        </div>
+        <div class="panel-body">
+          <div class="form-group">
+            <label>Pseudo</label>
+            <input type="text" name="pseudo" class = "form-control">
+          </div>
+          <div class="form-group">
+            <label>Email</label>
+            <input type="text" name="email" class = "form-control">
+          </div>
+        </div>
+      </div>
+
+      <div class="panel panel-default panel-insoumis">
+        <div class="panel-heading">
+          <h3 class = "panel-title">Proposition</h3>
+        </div>
+        <div class="panel-body">
+
+          <div class="form-group">
+            <label for="">Titre</label>
+            <input type="text" name="titre" class = "form-control">
+          </div>
+
+          <div class="form-group">
+            <label for="">Mots clés <small>(séparés par une virgule)</small></label>
+            <input type="text" name="titre" class = "form-control">
+          </div>
+
+          <div class="form-group">
+            <label for="">Contenu</label>
+            <textarea name="contenu" class = "form-control"></textarea>
+          </div>
+
+          <div class="form-group">
+            <input type="checkbox" >
+            <label for="">J'accepte les conditions d'utilisation de la plateforme.</label>
+          </div>
+
+        </div>
+      </div>
+      <!-- .panel (proposition) -->
+
+      <div class="form-group">
+        <button type="submit" class = "btn btn-default btn-insoumis form-control">Envoyer</button>
+      </div>
+    </form>
+
+
+  </div>
+  <!-- .col ... -->
+
+</div>
