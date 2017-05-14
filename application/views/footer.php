13bf4491499d97d7dc678c60dbbf3760bccd1d6b
MarcusMac <marc.muller@marcosoft.fr>
2017-05-14 19:44:05 +0200

    Modification du footer 	modified:   application/views/footer.php 	modified:   assets/css/styles.css

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,2 +1,10 @@
+
+
+    <footer>
+      <p>La Plateforme constituante des Insoumis - 2017</p>
+      <p>par <b>Les UnsConscients</b></p>
+      <p>Accès Modérateurs</p>
+    </footer>
+
   </body>
 </html>
