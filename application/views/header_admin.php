fc4a985f91f788a582d67bf2ecc5b0514dee4402
MarcusMac <marc.muller@marcosoft.fr>
2017-05-15 19:59:13 +0200

    modified:   application/views/header.php 	modified:   application/views/header_admin.php

--- application/views/header_admin.php
+++ application/views/header_admin.php
@@ -28,7 +28,7 @@
       <div class="container-fluid">
 
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-collapse" aria-expanded="false">
           <span class="sr-only">Toggle navigation</span>
           <span class="icon-bar"></span>
           <span class="icon-bar"></span>
@@ -37,7 +37,7 @@
           <a class="navbar-brand" href="#"></a>
         </div>
 
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+        <div class="collapse navbar-collapse" id="bs-collapse">
 
           <ul class = "nav navbar-nav">
 
