d94b42071f68e83268e9d91b8dd0fbb270dea8c0
MarcusMac <marc.muller@marcosoft.fr>
2017-05-17 20:52:35 +0200

    modified:   application/views/consulter_proposition.php

--- application/views/consulter_proposition.php
+++ application/views/consulter_proposition.php
@@ -1,11 +1,52 @@
+
+
 <div class="container">
 
-  <div class="panel-header">
-    <h1><?php echo $titre; ?></h1>
-  </div>
+  <div class="col-lg-8 col-lg-offset-2 col-xs-12">
 
-  <div class="content-proposition">
-    <?php echo $contenu; ?>
+    <div class="panel panel-proposition">
+
+      <div class="panel-heading">
+
+        <!-- <div class="post-heading"> -->
+
+          <div class="pull-left image">
+            <img src = "<?php echo 'https://www.gravatar.com/avatar/'.$gravatar_hash; ?>" class="img-circle avatar" >
+          </div>
+          <div class="pull-left meta">
+            <div class="title h5">
+              Proposition de <a href="#"><b><?php echo $auteur_pseudo; ?></b><small> (<?php echo $auteur_email; ?>)</small></a>
+            </div>
+            <h6 class="text-muted time">Le <?php echo $_date; ?></h6>
+          </div>
+
+        <!-- </div> -->
+
+      </div>
+
+      <div class="panel-body">
+
+        <h2><?php echo $titre; ?></h2>
+
+        <div class="contenu">
+          <?php echo $contenu; ?>
+        </div>
+
+      </div>
+      <div class="panel-footer">
+
+        <div class="pull-right">
+
+          <a href = "<?php echo base_url('admin/autorise/proposition/'.$id.'/oui'); ?>" class="btn btn-success">Oui</a>
+          <a href = "<?php echo base_url('admin/autorise/proposition/'.$id.'/non'); ?>" class="btn btn-danger">Non </a>
+          <a href = "" class="btn btn-default"> <span class = "glyphicon glyphicon-envelope"></span> Contacter l'auteur </a>
+
+        </div>
+
+        <h4>Autoriser ? </h4>
+
+      </div>
+    </div>
   </div>
 
 </div>
