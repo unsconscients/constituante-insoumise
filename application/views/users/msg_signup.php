dbc77b5ecc02a4404bb84efa782d606e6081eb71
MarcusMac <marc.muller@marcosoft.fr>
2017-05-20 16:43:51 +0200

    new file:   application/views/users/msg_signup.php

---
+++ application/views/users/msg_signup.php
@@ -0,0 +1,10 @@
+<div class="container">
+  <div class="col-lg-8 col-lg-offset-2 col-xs-12">
+
+    <div class="jumbotron jumbotron-insoumis">
+      <h1>Votre compte a été créé !</h1>
+      <p>Avant de pouvoir l'utiliser, consultez votre boite mail pour confirmer votre compte. </p>
+    </div>
+
+  </div>
+</div>
