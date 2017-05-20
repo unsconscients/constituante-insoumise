107b46b216e93063c2c97479f5630aa1133ed051
MarcusMac <marc.muller@marcosoft.fr>
2017-05-20 18:47:29 +0200

    modified:   application/config/config.php

--- application/config/config.php
+++ application/config/config.php
@@ -35,7 +35,7 @@ $config['base_url'] = 'http://lappel-presse.fr/constituante-insoumise/';
 | variable so that it is blank.
 |
 */
-$config['index_page'] = 'index.php';
+$config['index_page'] = '';
 
 /*
 |--------------------------------------------------------------------------
