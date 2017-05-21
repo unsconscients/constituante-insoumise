2ff68b76497da25c8206831952f44b5849fdfb9b
MarcusMac <marc.muller@marcosoft.fr>
2017-05-21 18:32:44 +0200

    modified:   application/views/soutenir_proposition.php

--- application/views/soutenir_proposition.php
+++ application/views/soutenir_proposition.php
@@ -36,12 +36,12 @@
 
         <div class="pull-right">
 
-          <a href = "<?php echo base_url('/soutenir/proposition/'.$proposition['id'].'/pour'); ?>" class="btn btn-success"><span class = "fa fa-thumbs-up"></span> <?php echo $proposition['pour']?>
+          <a href = "<?php echo base_url('/soutenir/proposition/'.$proposition['id'].'/pour'); ?>" class="btn btn-success"><span class = "glyphicon glyphicon-thumbs-up"></span> <?php echo $proposition['pour']?>
             <?php if(isset($proposition['vote_user_pour']) && $proposition['vote_user_pour'] != null && $proposition['vote_user_pour'] == 1) : ?>
               <small>(vous)</small>
             <?php endif;?>
           </a>
-          <a href = "<?php echo base_url('/soutenir/proposition/'.$proposition['id'].'/contre'); ?>" class="btn btn-danger"><span class = "fa fa-thumbs-down"></span> <?php echo $proposition['contre']?>
+          <a href = "<?php echo base_url('/soutenir/proposition/'.$proposition['id'].'/contre'); ?>" class="btn btn-danger"><span class = "glyphicon glyphicon-thumbs-down"></span> <?php echo $proposition['contre']?>
             <?php if(isset($proposition['vote_user_contre']) && $proposition['vote_user_contre'] != null && $proposition['vote_user_contre'] == 1) : ?>
               <small>(vous)</small>
             <?php endif;?>
