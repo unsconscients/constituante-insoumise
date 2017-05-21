6e872310e44471bd2760876eea73961a867ea5a9
MarcusMac <marc.muller@marcosoft.fr>
2017-05-21 10:07:21 +0200

    modified:   application/controllers/Commentaires.php

--- application/controllers/Commentaires.php
+++ application/controllers/Commentaires.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Msg extends CI_Controller {
+class Commentaires extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
